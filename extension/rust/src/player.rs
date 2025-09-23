95c5e38f8757d2e6660c7451cd044e4a8a9b0967
hortensia <824466875@qq.com>
2025-09-23 21:06:44 +0800

    feat: add player movement by key

--- extension/rust/src/player.rs
+++ extension/rust/src/player.rs
@@ -1,47 +1,28 @@
-use godot::classes::Sprite2D;
+use godot::classes::{CharacterBody2D, ICharacterBody2D, Input};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=CharacterBody2D)]
 struct Player {
     speed: f64,
-    angular_speed: f64,
 
-    base: Base<Sprite2D>,
+    base: Base<CharacterBody2D>,
 }
 
-use godot::classes::ISprite2D;
-
 #[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
+impl ICharacterBody2D for Player {
+    fn init(base: Base<CharacterBody2D>) -> Self {
         godot_print!("Hello, world!"); // Prints to the Godot console
 
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
+        Self { speed: 400.0, base }
     }
 
     fn physics_process(&mut self, delta: f64) {
-        // GDScript code:
-        //
-        // rotation += angular_speed * delta
-        // var velocity = Vector2.UP.rotated(rotation) * speed
-        // position += velocity * delta
+        let input_direction =
+            Input::singleton().get_vector("move_left", "move_right", "move_up", "move_down");
 
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+        let delta_velocity = input_direction * self.speed as f32 * delta as f32;
 
-        let rotation = self.base().get_rotation();
-        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
-        self.base_mut().translate(velocity * delta as f32);
-
-        // or verbose:
-        // let this = self.base_mut();
-        // this.set_position(
-        //     this.position() + velocity * delta as f32
-        // );
+        self.base_mut().move_and_collide(delta_velocity);
     }
 }
